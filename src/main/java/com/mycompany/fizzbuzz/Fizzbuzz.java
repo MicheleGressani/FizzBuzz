78e04223b635e8547ae36bf18fc2fd6e6c3c2d2e
Michele Gressani <michele.gressani@gmail.com>
2022-10-24 14:53:31 +0200

    prima implementazione fizzbuzz

--- src/main/java/com/mycompany/fizzbuzz/Fizzbuzz.java
+++ src/main/java/com/mycompany/fizzbuzz/Fizzbuzz.java
@@ -5,13 +5,52 @@
 
 package com.mycompany.fizzbuzz;
 
+import java.util.*;
 /**
  *
  * @author utente
  */
-public class Fizzbuzz {
+public class Fizzbuzz 
+{
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+        Scanner tastiera= new Scanner(System.in);
+        
+        int numero;
+        
+        System.out.println("Inserisci numero: ");
+        numero=tastiera.nextInt();
+        do
+        {
+            numero++;
+            
+            if(numero%3==0 && numero%5==0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            
+            else if(numero%3==0 || numero%5==0)
+            {
+                if(numero%3==0)
+                {
+                    System.out.println("Fizz");
+                }
+                
+                else if(numero%5==0)
+                {
+                    System.out.println("Buzz");
+                }
+            }
+            
+            else
+            {
+                System.out.println(numero);
+            }
+            
+        }while(numero<100);
+        
+        
+        
     }
 }
